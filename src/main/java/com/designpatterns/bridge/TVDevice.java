3a808f1dbbdf084a3e7d6978ca866711bc897561
amailth <anbuathypathy@gmail.com>
2023-11-11 10:09:20 -0500

    Bridge DP

---
+++ src/main/java/com/designpatterns/bridge/TVDevice.java
@@ -0,0 +1,18 @@
+package com.designpatterns.bridge;
+
+public class TVDevice implements Device{
+    @Override
+    public void turnOn() {
+        System.out.println("TV Turned on");
+    }
+
+    @Override
+    public void turnOff() {
+        System.out.println("TV Turned off");
+    }
+
+    @Override
+    public void setChannel(int channel) {
+        System.out.println("Switched TV Channel to "+ channel);
+    }
+}
