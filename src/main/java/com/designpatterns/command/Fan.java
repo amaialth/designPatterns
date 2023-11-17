5f24343f93316a1f255781077c8b1f6b3c5eb5f3
amailth <anbuathypathy@gmail.com>
2023-11-17 18:40:45 -0500

    Command DP

---
+++ src/main/java/com/designpatterns/command/Fan.java
@@ -0,0 +1,13 @@
+package com.designpatterns.command;
+
+//Receiver
+public class Fan {
+    public void turnOn(){
+        System.out.println("Fan is ON");
+    }
+
+    public void turnOff(){
+        System.out.println("Fan is OFF");
+    }
+
+}
---
+++ src/main/java/com/designpatterns/command/FanOnCommand.java
@@ -0,0 +1,14 @@
+package com.designpatterns.command;
+
+public class FanOnCommand implements Command{
+    private Fan fan;
+
+    public FanOnCommand(Fan fan) {
+        this.fan = fan;
+    }
+
+    @Override
+    public void execute() {
+        fan.turnOn();
+    }
+}
